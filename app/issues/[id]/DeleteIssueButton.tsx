2259d1530c4eb782ac4879710f689bf950231409
Juliana Ifionu <julieifionu@gmail.com>
2024-06-19 10:48:16 -0400

    Add dialog box when deleting an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,15 +1,38 @@
-import { Button, Flex } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { IoTrashOutline } from 'react-icons/io5';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 	return (
 		<>
-			<Button size={'3'} color="red">
-				<Flex justify={'center'} align={'center'} gap={'3'}>
-					<IoTrashOutline />
-					Delete
-				</Flex>
-			</Button>
+			<AlertDialog.Root>
+				<AlertDialog.Trigger>
+					<Button size={'3'} color="red">
+						<Flex justify={'center'} align={'center'} gap={'3'}>
+							<IoTrashOutline />
+							Delete
+						</Flex>
+					</Button>
+				</AlertDialog.Trigger>
+				<AlertDialog.Content maxWidth="450px">
+					<AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+					<AlertDialog.Description size="2">
+						Are you sure? This issue will be deleted and this action is
+						irreversible
+					</AlertDialog.Description>
+					<Flex gap="3" mt="4" justify="end">
+						<AlertDialog.Cancel>
+							<Button variant="soft" color="gray">
+								Cancel
+							</Button>
+						</AlertDialog.Cancel>
+						<AlertDialog.Action>
+							<Button variant="solid" color="red">
+								Confirm
+							</Button>
+						</AlertDialog.Action>
+					</Flex>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
 		</>
 	);
 };

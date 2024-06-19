4ed3220362ccfeedcc682ccc1a4eda6b56f5a5f4
Juliana Ifionu <julieifionu@gmail.com>
2024-06-19 12:13:15 -0400

    Handle deletion errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,16 +2,28 @@
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { IoTrashOutline } from 'react-icons/io5';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 	const router = useRouter();
+	const [error, setError] = useState(false);
+
+	const deleteIssue = async () => {
+		try {
+			await axios.delete(`/api/issues/${issueId}`);
+			router.push('/issues');
+			router.refresh();
+		} catch (error) {
+			setError(true);
+		}
+	};
 
 	return (
 		<>
 			<AlertDialog.Root>
 				<AlertDialog.Trigger>
-					<Button size={'3'} color="red">
+					<Button className="cursor-pointer" size={'3'} color="red">
 						<Flex justify={'center'} align={'center'} gap={'3'}>
 							<IoTrashOutline />
 							Delete
@@ -26,24 +38,43 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 					</AlertDialog.Description>
 					<Flex gap="3" mt="4" justify="end">
 						<AlertDialog.Cancel>
-							<Button variant="soft" color="gray">
+							<Button className="cursor-pointer" variant="soft" color="gray">
 								Cancel
 							</Button>
 						</AlertDialog.Cancel>
 						<AlertDialog.Action>
 							<Button
+								className="cursor-pointer"
 								variant="solid"
 								color="red"
-								onClick={async () => {
-									await axios.delete(`/api/issues/${issueId}`);
-									router.push('/issues');
-									router.refresh();
-								}}
+								onClick={deleteIssue}
 							>
 								Confirm
 							</Button>
 						</AlertDialog.Action>
 					</Flex>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+
+			<AlertDialog.Root open={error}>
+				<AlertDialog.Content maxWidth="450px">
+					<AlertDialog.Title>Error</AlertDialog.Title>
+					<AlertDialog.Description size="2">
+						Issue could not be deleted, try again later
+					</AlertDialog.Description>
+					<AlertDialog.Action>
+						<Flex justify="end">
+							<Button
+								className="cursor-pointer"
+								mt={'4'}
+								variant="soft"
+								color="gray"
+								onClick={() => setError(false)}
+							>
+								Ok
+							</Button>
+						</Flex>
+					</AlertDialog.Action>
 				</AlertDialog.Content>
 			</AlertDialog.Root>
 		</>

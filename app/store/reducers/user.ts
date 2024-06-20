c590ce7ccdaac1ee9d2dc4c91b73c6435e99d61e
Juliana Ifionu <julieifionu@gmail.com>
2024-06-20 18:19:58 -0400

    Configure redux store for state management

---
+++ app/store/reducers/user.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { UserObj, UserState } from '@/app/utility/dataTypes';
+
+const initialState: UserState = {
+	userData: null,
+};
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {
+		setUser: (state, action: PayloadAction<UserObj>) => {
+			state.userData = action.payload;
+		},
+	},
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;

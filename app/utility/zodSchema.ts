2a41b5627343ced1db4d06ffc07b8c63097d2376
Juliana Ifionu <julieifionu@gmail.com>
2024-06-20 18:16:13 -0400

    Add user validation type

--- app/utility/zodSchema.ts
+++ app/utility/zodSchema.ts
@@ -4,3 +4,9 @@ export const IssueSchema = z.object({
 	title: z.string().min(3, 'Title must be more than 3 chars').max(255),
 	description: z.string().min(3, 'Description must be more than 3 chars'),
 });
+
+export const userSchema = z.object({
+	name: z.string().min(5),
+	email: z.string().min(5),
+	password: z.string().min(5)
+})

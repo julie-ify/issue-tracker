7950931adb9f4b36d00c9004d4408c5ca427f194
Juliana Ifionu <julieifionu@gmail.com>
2024-06-20 21:10:19 -0400

    Create utility functions for user auth

---
+++ app/utility/auth.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+export const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+export const verifyPassword = async (password: string, hashedPassword: string) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+export const generateToken = (userId: string) => {
+  return jwt.sign({ userId }, SECRET_KEY!, { expiresIn: '1h' });
+};
+
+export const verifyToken = (token: string) => {
+  return jwt.verify(token, SECRET_KEY!);
+};

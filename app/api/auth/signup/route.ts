433665f7abac1f4ba7ab6964e4672cfe6b9dd088
Juliana Ifionu <julieifionu@gmail.com>
2024-06-20 21:13:34 -0400

    Set up JWT as httponly cookie for authenticating users

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,7 +1,8 @@
 import { userSchema } from '@/app/utility/zodSchema';
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
-import bcrypt from 'bcrypt';
+import { generateToken, hashPassword } from '@/app/utility/auth';
+import { setCookie } from '@/app/utility/cookies';
 
 export async function POST(request: NextRequest) {
 	const body = await request.json();
@@ -17,20 +18,44 @@ export async function POST(request: NextRequest) {
 	});
 
 	if (existingUser)
-		return NextResponse.json({ error: 'Invalid credentials' }, { status: 400 });
+		return NextResponse.json(
+			{ error: 'Invalid email or password' },
+			{ status: 401 }
+		);
 
-	const securePassword = await bcrypt.hash(body.password, 10);
+	const hashedPassword = await hashPassword(body.password);
 	const user = await prisma.user.create({
 		data: {
 			name: body.name,
 			email: body.email,
-			password: securePassword,
+			password: hashedPassword,
 		},
 	});
 
+	if (!user) {
+		return NextResponse.json(
+			{ error: 'An unexpected error occurred' },
+			{ status: 401 }
+		);
+	}
+
+	const token = generateToken(user.id);
+	if (!token)
+		return NextResponse.json(
+			{ error: 'An unexpected error occurred' },
+			{ status: 401 }
+		);
+
 	const { name, email, role, createdAt } = user;
+	const responseData = { name, email, role, createdAt };
 
-	const formatedUser = { name, email, role, createdAt };
+	const response = NextResponse.json(responseData, { status: 201 });
 
-	return NextResponse.json(formatedUser, { status: 201 });
+	setCookie(response, 'token', token, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === 'production',
+		maxAge: 60 * 60 * 1000, // max age in milliseconds
+		path: '/',
+	});
+	return response;
 }

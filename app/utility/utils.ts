03d41f1b466ff174e57319fe32c42207556d335c
Juliana Ifionu <julieifionu@gmail.com>
2024-06-23 21:54:49 -0400

    Create func to extract first letter

--- app/utility/utils.ts
+++ app/utility/utils.ts
@@ -2,3 +2,7 @@ export const isValidId = (id: string) => {
 	// Check if `id` is a string containing only digits
 	return /^\d+$/.test(id);
 };
+
+export const extractFirstLetter = (name: string) => {
+	return name.split('')[0].toUpperCase();
+};

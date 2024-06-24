a1e9fc6923eb42800a34dfb5aaad7bce461cab8f
Juliana Ifionu <julieifionu@gmail.com>
2024-06-24 19:07:17 -0400

    Use react query to manage users data fetching

--- app/issues/[id]/AssignIssue.tsx
+++ app/issues/[id]/AssignIssue.tsx
@@ -1,24 +1,32 @@
 'use client';
+import Skeleton from '@/app/components/Skeleton';
 import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 
 const AssignIssue = () => {
-	const [users, setUsers] = useState<User[]>([]);
+	// with react query, no need of custom error handler, state management and useEffect. It handles that
+	const {
+		data: users,
+		error,
+		isLoading,
+	} = useQuery<User[]>({
+		queryKey: ['users'], // cache unique identifier
+		queryFn: () => axios.get('/api/auth/users').then((res) => res.data),
+		staleTime: 60 * 1000, // refetch after 60s
+		retry: 3, // retry 3 more times if the first fails
+	});
 
-	useEffect(() => {
-		const fetchUsers = async () => {
-			const { data } = await axios.get('/api/auth/users');
-			setUsers(data);
-		};
-		fetchUsers();
-	}, []);
+	if (isLoading) return <Skeleton />;
+
+	if (error) return null;
+
 	return (
-		<Select.Root size="3" defaultValue='Assign issue'>
+		<Select.Root size="3" defaultValue="Assign issue">
 			<Select.Trigger className="cursor-pointer" />
 			<Select.Content>
-				{users.map((user) => (
+				{users?.map((user) => (
 					<Select.Item key={user.id} value={user.id} className="cursor-pointer">
 						{user.name}
 					</Select.Item>

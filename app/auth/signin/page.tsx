8f83e5572319599e7ccdf0ab0f6d880f968635d3
Juliana Ifionu <julieifionu@gmail.com>
2024-06-23 22:48:58 -0400

    Style sign up and sign in pages

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,7 +3,7 @@ import { ErrorHandler } from '@/app/components';
 import { setUser } from '@/app/store/reducers/user';
 import { signinSchema } from '@/app/utility/zodSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Grid, TextField } from '@radix-ui/themes';
+import { Button, Grid, TextField, Text, Box } from '@radix-ui/themes';
 import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -33,37 +33,42 @@ const Signin = () => {
 	};
 
 	return (
-		<>
-			<div className="max-w-full flex gap-3">
-				<form
-					className="max-w-full flex-1 w-32"
-					onSubmit={handleSubmit(onSubmit)}
-				>
-					<Grid gap="3" columns={{ initial: '1', sm: '1' }}>
+		<div className="max-w-80 m-auto space-y-5 h-full mt-20">
+			<Text size={'6'} className='flex items-center justify-center font-bold'>Sign In</Text>
+			<form
+				className="max-w-full border p-5 space-y-5"
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<Grid gap="5">
+					<Box className="space-y-2">
+						<Text size={'2'}>Full Name</Text>
 						<TextField.Root
-							size="2"
-							placeholder="Email"
+							size="3"
+							placeholder="Enter your email address"
 							{...register('email')}
 						/>
+					</Box>
+					<Box className="space-y-2">
+						<Text size={'2'}>Password</Text>
 						<TextField.Root
-							size="2"
-							placeholder="Password"
+							size="3"
+							placeholder="Enter your password"
 							{...register('password')}
 						/>
-						{(errors.email || errors.password) && (
-							<ErrorHandler>All the fields are required!</ErrorHandler>
-						)}
-						<Button type="submit">Signin</Button>
-					</Grid>
-				</form>
-			</div>
-			<div className="text-cyan-500">
-				If you don't have account?{' '}
-				<Link href="/auth/signup" className="cursor-pointer underline">
-					Sign up
-				</Link>
-			</div>
-		</>
+					</Box>
+					{(errors.email || errors.password) && (
+						<ErrorHandler>All the fields are required!</ErrorHandler>
+					)}
+					<Button className='cursor-pointer' type="submit" size={'3'}>Sign in</Button>
+				</Grid>
+				<div className="text-cyan-500">
+					If you don't have account?{' '}
+					<Link href="/auth/signup" className="cursor-pointer underline">
+						Sign up
+					</Link>
+				</div>
+			</form>
+		</div>
 	);
 };
 
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -3,8 +3,9 @@ import { ErrorHandler } from '@/app/components';
 import { setUser } from '@/app/store/reducers/user';
 import { signupSchema } from '@/app/utility/zodSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Grid, TextField } from '@radix-ui/themes';
+import { Button, Grid, TextField, Text, Box } from '@radix-ui/themes';
 import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
@@ -32,28 +33,53 @@ const Signup = () => {
 	};
 
 	return (
-		<div className="max-w-full flex gap-3 h-64">
+		<div className="max-w-80 m-auto space-y-5 h-full mt-20">
+			<Text size={'6'} className="flex items-center justify-center font-bold">
+				Sign Up
+			</Text>
 			<form
-				className="max-w-full flex-1 w-32"
+				className="max-w-full border p-5 space-y-5"
 				onSubmit={handleSubmit(onSubmit)}
 			>
-				<Grid gap="3" columns={{ initial: '1', sm: '1' }}>
-					<TextField.Root
-						size="2"
-						placeholder="Full Name"
-						{...register('name')}
-					/>
-					<TextField.Root size="2" placeholder="Email" {...register('email')} />
-					<TextField.Root
-						size="2"
-						placeholder="Password"
-						{...register('password')}
-					/>
+				<Grid gap="5" columns={{ initial: '1', sm: '1' }}>
+					<Box className="space-y-2">
+						<Text size={'2'}>Full Name</Text>
+						<TextField.Root
+							size="3"
+							placeholder="Enter your name"
+							{...register('name')}
+						/>
+					</Box>
+					<Box className="space-y-2">
+						<Text size={'2'}>Email</Text>
+						<TextField.Root
+							size="3"
+							placeholder="Enter your email address"
+							{...register('email')}
+						/>
+					</Box>
+					<Box className="space-y-2">
+						<Text size={'2'}>Password</Text>
+						<TextField.Root
+							size="3"
+							placeholder="Enter your password"
+							{...register('password')}
+						/>
+					</Box>
+
 					{(errors.name || errors.email || errors.password) && (
 						<ErrorHandler>All the fields are required!</ErrorHandler>
 					)}
-					<Button type="submit">Signup</Button>
+					<Button type="submit" size={'3'} className='cursor-pointer'>
+						Sign up
+					</Button>
 				</Grid>
+				<div className="text-cyan-500">
+					If you have an account?{' '}
+					<Link href="/auth/signin" className="cursor-pointer underline">
+						Sign in
+					</Link>
+				</div>
 			</form>
 		</div>
 	);

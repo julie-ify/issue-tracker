382e960fa8884c8a7c95c3d010df3e465cdcab32
Juliana Ifionu <julieifionu@gmail.com>
2024-06-24 00:39:04 -0400

    List users to assign tasks

--- app/issues/[id]/AssignIssue.tsx
+++ app/issues/[id]/AssignIssue.tsx
@@ -1,14 +1,28 @@
-'use client'
+'use client';
+import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const AssignIssue = () => {
+	const [users, setUsers] = useState<User[]>([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const { data } = await axios.get('/api/auth/users');
+			setUsers(data);
+		};
+		fetchUsers();
+	}, []);
 	return (
-		<Select.Root size="3" defaultValue="julie">
-			<Select.Trigger className='cursor-pointer'/>
+		<Select.Root size="3" defaultValue='Assign isse'>
+			<Select.Trigger className="cursor-pointer" />
 			<Select.Content>
-				<Select.Item value="julie" className='cursor-pointer'>Julie</Select.Item>
-				<Select.Item value="mary" className='cursor-pointer'>Mary</Select.Item>
+				{users.map((user) => (
+					<Select.Item key={user.id} value={user.id} className="cursor-pointer">
+						{user.name}
+					</Select.Item>
+				))}
 			</Select.Content>
 		</Select.Root>
 	);

e58ecfe9c4691e57f994b1e56393652f3d6b884d
Juliana Ifionu <julieifionu@gmail.com>
2024-06-23 21:52:52 -0400

    Add sign up link in the sign in page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -5,6 +5,7 @@ import { signinSchema } from '@/app/utility/zodSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Grid, TextField } from '@radix-ui/themes';
 import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
@@ -32,25 +33,37 @@ const Signin = () => {
 	};
 
 	return (
-		<div className="max-w-full flex gap-3 h-64">
-			<form
-				className="max-w-full flex-1 w-32"
-				onSubmit={handleSubmit(onSubmit)}
-			>
-				<Grid gap="3" columns={{ initial: '1', sm: '1' }}>
-					<TextField.Root size="2" placeholder="Email" {...register('email')} />
-					<TextField.Root
-						size="2"
-						placeholder="Password"
-						{...register('password')}
-					/>
-					{(errors.email || errors.password) && (
-						<ErrorHandler>All the fields are required!</ErrorHandler>
-					)}
-					<Button type="submit">Signin</Button>
-				</Grid>
-			</form>
-		</div>
+		<>
+			<div className="max-w-full flex gap-3">
+				<form
+					className="max-w-full flex-1 w-32"
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<Grid gap="3" columns={{ initial: '1', sm: '1' }}>
+						<TextField.Root
+							size="2"
+							placeholder="Email"
+							{...register('email')}
+						/>
+						<TextField.Root
+							size="2"
+							placeholder="Password"
+							{...register('password')}
+						/>
+						{(errors.email || errors.password) && (
+							<ErrorHandler>All the fields are required!</ErrorHandler>
+						)}
+						<Button type="submit">Signin</Button>
+					</Grid>
+				</form>
+			</div>
+			<div className="text-cyan-500">
+				If you don't have account?{' '}
+				<Link href="/auth/signup" className="cursor-pointer underline">
+					Sign up
+				</Link>
+			</div>
+		</>
 	);
 };
 

9c73e9c528450b497ff2859fb44ac22b52768390
Juliana Ifionu <julieifionu@gmail.com>
2024-06-23 20:38:45 -0400

    Fetch logged in user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
+'use client';
+import { useEffect } from 'react';
+import { handleError } from './components/handleError';
+import { errorNotice } from './components/Toast';
+import { setUser } from './store/reducers/user';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './store';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+	const isUser = useSelector((state: RootState) => state.user.userData);
+	const dispatch = useDispatch();
+	const router = useRouter();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const response = await fetch('/api/auth/user');
+				const user = await response.json();
+				dispatch(setUser(user));
+				router.refresh();
+			} catch (error) {
+				const errorMsg = handleError(error);
+				errorNotice(errorMsg);
+				dispatch(setUser(null));
+			}
+		};
+		fetchUser();
+	}, []);
+
+	if (!isUser) {
+		return <p>Loading...</p>;
+	}
+
 	return (
 		<div>
-			<h1>Hello world</h1>
+			<div>
+				Hello {isUser && <h2>{isUser?.name}</h2>} {!isUser && <h2>There</h2>}
+			</div>
 		</div>
 	);
 }
